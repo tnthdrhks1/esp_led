ea277809f05009e81e042ac5ee807f38aba9f0cf
lsj <dltmdwn021@kangwon.ac.kr>
2020-09-17 14:31:15 +0900

    esp-led

---
+++ 20200701_led.ino
@@ -0,0 +1,13 @@
+#define led 22
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+  Serial.println("data : 101");
+  
+}
---
+++ 20200701_register_led.ino/20200701_register_led.ino.ino
@@ -0,0 +1,33 @@
+#define LED 22
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+ 
+// setting PWM properties
+const int freq = 5000;
+const int ledChannel = 2;
+const int resolution = 10; //Resolution 8, 10, 12, 15
+ 
+//=======================================================================
+//                    Power on setup
+//=======================================================================
+void setup() {
+  pinMode(LED,OUTPUT);
+  
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannel, freq, resolution);
+  
+  // attach the channel to the GPIO2 to be controlled
+  ledcAttachPin(LED, ledChannel);
+}
+ 
+//=======================================================================
+//                    Main Program Loop
+//=======================================================================
+void loop() {
+  
+  //PWM Value varries from 0 to 1023  
+  while(1)
+  {
+      digitalWrite(LED, HIGH);
+  }
+}
